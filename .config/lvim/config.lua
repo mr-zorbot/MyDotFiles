2f2e2829f29d4fcbfd21f0592e8db49d3b4d6dcb
Iann Zorkot <78239166+mr-zorbot@users.noreply.github.com>
2025-02-24 23:58:10 +0000

    Update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -6,40 +6,27 @@ lvim.colorscheme = "dracula"
 vim.opt.wrap = true
 
 lvim.plugins = {
-  "Mofiqul/dracula.nvim",
-  "p00f/clangd_extensions.nvim",
-  "mfussenegger/nvim-jdtls",
-  "olexsmir/gopher.nvim",
-  "sbdchd/neoformat",
-  {
-  "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
+    "Mofiqul/dracula.nvim", "p00f/clangd_extensions.nvim",
+    "mfussenegger/nvim-jdtls", "olexsmir/gopher.nvim", "sbdchd/neoformat", {
+        "kdheepak/lazygit.nvim",
+        lazy = false,
+        cmd = {
+            "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter",
+            "LazyGitFilterCurrentFile"
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"
+        },
+        config = function()
+            require("telescope").load_extension("lazygit")
+        end
     }
-  }
 }
 
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "cpp",
-    "go",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
+    "bash", "c", "cpp", "go", "javascript", "json", "lua", "python",
+    "typescript", "tsx", "css", "rust", "java", "yaml"
 }
 
 lvim.lsp.installer.setup.ensure_installed = {
@@ -54,18 +41,30 @@ lvim.lsp.installer.setup.ensure_installed = {
     "grammarly", -- Markdown
     "pyright", -- Python
     "lemminx", -- XML
-    "yamlls", -- Yaml 
-  }
+    "yamlls" -- Yaml 
+}
 
+-- Habilitar o Neoformat 
+vim.g.neoformat_enabled = {
+    'bash', 'c', 'cpp', 'go', 'javascript', 'json', 'lua', 'python',
+    'typescript', 'tsx', 'css', 'rust', 'java', 'yaml'
+}
+
+-- Configuração para formatar ao salvar automaticamente
+vim.cmd [[autocmd BufWritePre * Neoformat]]
 
 -- Keymap para mapear a tecla F1 para executar o comando :Neoformat
-vim.api.nvim_set_keymap('n', '<F1>', ':Neoformat<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<F1>', ':Neoformat<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F1>', ':Neoformat<CR>',
+                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<F1>', ':Neoformat<CR>',
+                        {noremap = true, silent = true})
 
--- Keymap para mapear a tecla F2 para executar o comando :ChatGPT
-vim.api.nvim_set_keymap('n', '<F2>', ':ChatGPT<CR>', { noremap = true, silent = true })
+-- Keymap para mapear a tecla F2 para executar o comando :LazyGit
+vim.api.nvim_set_keymap('n', '<F2>', ':LazyGit<CR>',
+                        {noremap = true, silent = true})
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.ignore_install = {"haskell"}
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.lsp.installer.setup.automatic_installation = true
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd", "jdtls", "gopls",})
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers,
+                {"clangd", "jdtls", "gopls"})
