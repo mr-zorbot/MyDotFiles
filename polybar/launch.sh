15c4e9ca3793f6d5ae889207103fe78768a9dd2d
Iann Zorkot <78239166+mr-zorbot@users.noreply.github.com>
2025-03-23 17:13:48 -0300

    Delete polybar directory

--- polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-THEME="dracula"
-
-killall polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-CONFIG_DIR=$HOME/.config/polybar/themes/$THEME/config.ini
-polybar main -c $CONFIG_DIR &
